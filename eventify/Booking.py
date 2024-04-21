2c5b3bebbdebcec1283cbc76f4f1b4da0f1ab578
erick-sudo <erickochieng766@gmail.com>
2024-04-21 17:44:22 +0300

    Commit

---
+++ eventify/Booking.py
@@ -0,0 +1,6 @@
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.first_name} - {self.event.name}"
